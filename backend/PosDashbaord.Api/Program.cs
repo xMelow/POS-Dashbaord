7b9825a12090fc3718cf5a2deb634fe4b904aa47
Flor Stellamans <florstellamamans@gmail.com>
2026-08-28 10:51:49 +0200

    add gitignore file

--- backend/PosDashbaord.Api/Program.cs
+++ backend/PosDashbaord.Api/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+var connectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<AppDbContext>(connectionString);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
